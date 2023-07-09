e5eb471f5856517e8d6b2f78eeed9a240a9ad3b5
Ergo Oll <ergooll98@gmail.com>
2023-07-09 20:43:42 +0300

    Created code for book objects All books are stored in a array Books from said array will be displayed as elements

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
 let myLibrary = [
     new Book('Dune', 'Frank Herbert', 896, false),
-    new Book('Red Mars', 'Kim Stanley Robinson', 501, false)
+    new Book('Red Mars', 'Kim Stanley Robinson', 501, true)
 ];
+
+const libraryContainer = document.querySelector('.library-container');
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        let readText = 'not read yet';
-        if (read == true) {
-            readText = 'book has been read'
-        } else {
-            readText = 'not read yet'
-        }
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + readText;
+    
+}
+
+Book.prototype.info = function() {
+    let readText = 'not read yet';
+    let readColor = '#f24b4b';
+    if (this.read == true) {
+        readText = 'book has been read'
+        readColor = '#5bc359';
+    } else {
+        readText = 'not read yet'
+        readColor = '#f24b4b';
     }
+    // return `${this.title} by ${this.author}, ${this.pages} pages, ${readText}`;
+    return `
+        <h3>${this.title}</h3>
+        <p>by: ${this.author}</p>
+        <p>Pages: ${this.pages}</p>
+        <p style="color:${readColor}">${readText}</p>
+    `
 }
 
 function addBookToLibrary() {
     
 }
 
-console.log(myLibrary);
+function drawLibrary() {
+    myLibrary.forEach(obj => {
+        const bookItem = document.createElement('div');
+        bookItem.classList.add('book-item');
+        bookItem.innerHTML = obj.info();
+        libraryContainer.appendChild(bookItem);
+    });
+}
+
+drawLibrary();
